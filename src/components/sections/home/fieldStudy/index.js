3554ad6a08b482b2f7b6162b27b113c2958f47a8
Aligodu <wilrona@gmail.com>
2021-02-04 11:45:06 +0100

    update
    - Specialisation page
    - create query graphQl for single request for specialisation page

--- src/components/sections/home/fieldStudy/index.js
+++ src/components/sections/home/fieldStudy/index.js
@@ -12,7 +12,7 @@ const FieldStudySection = (props) => {
 
                 <div className="mt-5 md:mt-10 grid md:grid-cols-5 grid-cols-2 gap-x-3 md:gap-x-6 gap-y-4 md:gap-y-8 justify-center">
 
-                    {props.data.categories.nodes.map((item, i) => {
+                    {props.data.categories.nodes.slice(0, 15).map((item, i) => {
                         return <HomeFieldStudyCard title={item.name} slug={item.slug} image={item.logo} key={i}/>
                     })}
 
